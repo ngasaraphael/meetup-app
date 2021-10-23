54dc9932f6b2a6ed239b5783cac1ba4f256045e7
ngasaraphael <66059003+ngasaraphael@users.noreply.github.com>
2021-10-24 00:18:20 +0200

    charts implemented

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ import EventList from './EventList';
 import NumberOfEvents from './NumberOfEvents';
 import { WarningAlert } from './Alert';
 import WelcomeScreen from './WelcomeScreen';
+import {
+  ScatterChart,
+  Scatter,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+} from 'recharts';
 
 class App extends Component {
   state = {
@@ -62,6 +71,18 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter(
+        (event) => event.location === location
+      ).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined)
       return <div className='App' />;
@@ -77,6 +98,23 @@ class App extends Component {
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
+
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400}>
+          <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+            <CartesianGrid />
+            <XAxis type='category' dataKey='city' name='city' />
+            <YAxis
+              allowDecimals={false}
+              type='number'
+              dataKey='number'
+              name='number of events'
+            />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill='#8884d8' />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
 
         <WelcomeScreen
